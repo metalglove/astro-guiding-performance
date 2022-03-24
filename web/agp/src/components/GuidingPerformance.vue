082e6b8248ab3f47ee7cf0e518a0a11b4f89dc39
Mario Lucassen <Mario.lucassen@hotmail.com>
2022-03-24 13:48:10 +0100

    Fixed incorrect arc-sec label

--- web/agp/src/components/GuidingPerformance.vue
+++ web/agp/src/components/GuidingPerformance.vue
@@ -56,22 +56,29 @@ export default defineComponent({
       },
       {
         id: 1,
-        value: 'pixel/arc-secs'
+        value: 'arc-secs/pixel '
       },
       {
         id: 2,
-        value: 'pixel/arc-secs RMS (50 sec window)'
+        value: 'arc-secs/pixel  RMS (50 sec window)'
       }];
 
-    const defaultOptions = computed(() => {
+    const defaultOptions = (title: string) => {
       return {
         responsive: true,
         plugins: {
           title: {
             display: true,
-            text: "TITLE",
+            text: title,
             font: {
               size: 20
+            }
+          },
+          legend: {
+            labels: {
+              filter: function (legendItem: any, chartData: any) {
+                return !(legendItem.datasetIndex === 2 && selectedScale.value !== 'arc-secs/pixel  RMS (50 sec window)');
+              }
             }
           }
         },
@@ -101,33 +108,35 @@ export default defineComponent({
           }
         },
       };
-    });
+    };
 
     const mountAxesOptions = computed(() => {
-      const mo = defaultOptions.value;
-      mo.plugins.title.text = 'Mount Axes Chart';
+      const mo = defaultOptions('Mount Axes Chart');
+      // mo.plugins.title.text = ;
       return mo;
     });
     const cameraAxesOptions = computed(() => {
-      const mo = defaultOptions.value;
-      mo.plugins.title.text = 'Camera Axes Chart';
-      return mo;
+      const co = defaultOptions('Camera Axes Chart');
+      // mo.plugins.title.text = ;
+      return co;
     });
 
     interface LocalGuidingFrame {
       datetime: Date;
       dx: number;
       dy: number;
+      totalXY: number;
       ra: number;
       dec: number;
+      totalRADEC: number;
     }
 
     const scale = (x: LocalGuidingFrame[]) => {
       if (selectedScale.value === 'pixels') {
         return x;
-      } else if (selectedScale.value === 'pixel/arc-secs') {
+      } else if (selectedScale.value === 'arc-secs/pixel ') {
         return scaleToPixelScale(x);
-      } else if (selectedScale.value === 'pixel/arc-secs RMS (50 sec window)') {
+      } else if (selectedScale.value === 'arc-secs/pixel  RMS (50 sec window)') {
         let dataScaledToPixelScale = scaleToPixelScale(x);
         let newX: LocalGuidingFrame[] = [];
         const rmsWindow = 50 * 1000; // to milliseconds
@@ -137,8 +146,10 @@ export default defineComponent({
           datetime: lastFrameWindow,
           dx: 0,
           dy: 0,
+          totalXY: 0,
           ra: 0,
-          dec: 0
+          dec: 0,
+          totalRADEC: 0
         };
         let counter = 0;
 
@@ -148,8 +159,10 @@ export default defineComponent({
             currentGuidingFrameWindow.datetime = lastFrameWindow;
             currentGuidingFrameWindow.dx = Math.sqrt(currentGuidingFrameWindow.dx / counter);
             currentGuidingFrameWindow.dy = Math.sqrt(currentGuidingFrameWindow.dy / counter);
+            currentGuidingFrameWindow.totalXY = (currentGuidingFrameWindow.dx + currentGuidingFrameWindow.dy) / 2;
             currentGuidingFrameWindow.ra = Math.sqrt(currentGuidingFrameWindow.ra / counter);
             currentGuidingFrameWindow.dec = Math.sqrt(currentGuidingFrameWindow.dec / counter);
+            currentGuidingFrameWindow.totalRADEC = (currentGuidingFrameWindow.ra + currentGuidingFrameWindow.dec) / 2;
             newX = newX.concat(currentGuidingFrameWindow);
 
             lastFrameWindow = currentFrame.datetime;
@@ -157,8 +170,10 @@ export default defineComponent({
               datetime: lastFrameWindow,
               dx: 0,
               dy: 0,
+              totalXY: 0,
               ra: 0,
-              dec: 0
+              dec: 0,
+              totalRADEC: 0
             };
             counter = 1;
           }
@@ -174,58 +189,102 @@ export default defineComponent({
 
       function scaleToPixelScale(x: LocalGuidingFrame[]): LocalGuidingFrame[]  {
         return x.map(x => {
-          return {
-            datetime: x.datetime,
-            dx: x.dx*guidingSession.pixelScale,
-            dy: x.dy*guidingSession.pixelScale,
-            ra: x.ra*guidingSession.pixelScale,
-            dec: x.dec*guidingSession.pixelScale
-          };
+          x.dx *= guidingSession.pixelScale;
+          x.dy *= guidingSession.pixelScale;
+          x.ra *= guidingSession.pixelScale;
+          x.dec *= guidingSession.pixelScale;
+          return x;
         });
       }
     };
 
     const scaledData = computed(() => {
-      // select the needed properties
       let data: LocalGuidingFrame[] = guidingSession.guidingFrames.map((x) => {
         return { datetime: x.datetime, dx: x.dx, dy: x.dy, ra: x.RARawDistance, dec: x.DECRawDistance } as LocalGuidingFrame;
         });
       return scale(data);
     });
 
-    const cameraAxes = computed(() => ({
+    const cameraAxes = computed(() => {
+     const dataset = {
         labels: scaledData.value.map((d) => d.datetime.toLocaleTimeString()),
         datasets: [
           {
             data: scaledData.value.map((d) => d.dx),
             backgroundColor: ['blue'],
-            label: 'dx',
+            label: selectedScale.value !== 'arc-secs/pixel  RMS (50 sec window)' ? 'dx' : 'dx RMS',
           },
           {
             data: scaledData.value.map((d) => d.dy),
             backgroundColor: ['orange'],
-            label: 'dy',
+            label: selectedScale.value !== 'arc-secs/pixel  RMS (50 sec window)' ? 'dy' : 'dy RMS',
           },
+          {
+            data: scaledData.value.map((d) => d.totalXY),
+            backgroundColor: ['green'],
+            label: 'Total RMS',
+          }
         ],
-      }));
+      };
+      return dataset;
+    });
 
-    const mountAxes = computed(() => ({
-      labels: scaledData.value.map((d) => d.datetime.toLocaleTimeString()),
-      datasets: [
-        {
-          data: scaledData.value.map((d) => d.ra),
-          backgroundColor: ['blue'],
-          label: 'RA'
-        },
-        {
-          data: scaledData.value.map((d) => d.dec),
-          backgroundColor: ['orange'],
-          label: 'DEC'
-        },
-      ],
-    }));
+    const mountAxes = computed(() => {
+     const dataset =  {
+        labels: scaledData.value.map((d) => d.datetime.toLocaleTimeString()),
+        datasets: [
+          {
+            data: scaledData.value.map((d) => d.ra),
+            backgroundColor: ['blue'],
+            label: selectedScale.value !== 'arc-secs/pixel  RMS (50 sec window)' ? 'RA' : 'RA RMS',
+          },
+          {
+            data: scaledData.value.map((d) => d.dec),
+            backgroundColor: ['orange'],
+            label: selectedScale.value !== 'arc-secs/pixel RMS (50 sec window)' ? 'DEC' : 'DEC RMS',
+          },
+          {
+            data: scaledData.value.map((d) => d.totalRADEC),
+            backgroundColor: ['green'],
+            label: 'Total RMS',
+          }
+        ],
+      };
+      return dataset;
+    });
 
-    return { selectedScale, selectOptions, cameraAxes, mountAxes, mountAxesOptions, cameraAxesOptions };
+    const planText = (autorun: Autorun) => {
+      let lights = autorun.exposureEvents.filter((exposure => exposure.type === "Light"));
+      let biases = autorun.exposureEvents.filter((exposure => exposure.type === "Bias"));
+      let darks = autorun.exposureEvents.filter((exposure => exposure.type === "Dark"));
+      let flats = autorun.exposureEvents.filter((exposure => exposure.type === "Flat"));
+
+      let text = 'Plan: \n';
+      if (lights.length > 0) {
+        text = `Plan ${autorun.plan}: \n`;
+        groupBy(lights, (light: ExposureEvent) => light.integrationTime).forEach((grouped) => {
+          text += `Light: ${ grouped.length }x ${grouped[0]?.integrationTime}`;
+        });
+      }
+      if (biases.length > 0) {
+        groupBy(biases, (bias: ExposureEvent) => bias.integrationTime).forEach((grouped) => {
+          text += `Bias: ${ grouped.length }x ${grouped[0]?.integrationTime}`;
+        });
+      }
+      if (darks.length > 0) {
+        groupBy(darks, (dark: ExposureEvent) => dark.integrationTime).forEach((grouped) => {
+          text += `Dark: ${ grouped.length }x ${grouped[0]?.integrationTime}`;
+        });
+      }
+      if (flats.length > 0) {
+        groupBy(flats, (flat: ExposureEvent) => flat.integrationTime).forEach((grouped) => {
+          text += `Flat: ${ grouped.length }x ${grouped[0]?.integrationTime}`;
+        });
+      }
+      return text;
+    }
+
+    return { planText, autorunLog: logs.AutorunLog, guidingSession, selectedScale, selectOptions, cameraAxes, mountAxes, mountAxesOptions, cameraAxesOptions };
   },
 });
 </script>
