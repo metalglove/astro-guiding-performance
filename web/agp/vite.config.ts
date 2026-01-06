66164e706396f9b9d63aaef57e689bfc69bed75a
Mario Lucassen <mario.lucassen@hotmail.com>
2026-01-06 22:21:43 +0100

    refactor: migrate from Vue CLI to Vite, update dependencies and ESLint configuration
    - Changed build scripts to use Vite instead of Vue CLI.
    - Added Vite and Vite PWA plugin as dependencies.
    - Updated ESLint and TypeScript dependencies to their latest versions.
    - Created a new Vite configuration file for project setup.
    - Adjusted ESLint configuration to extend from @typescript-eslint/recommended.

---
+++ web/agp/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  server: {
+    host: '0.0.0.0'
+  }
+})
